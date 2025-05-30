201e82a5836c0363101967ed672ac5142712182e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-05-31 00:20:24 +0530

    Flutter: Project-2: Results Page Left, Rest Development Is Complete

---
+++ Project-2-Flutter-Quiz-App/flutter_quiz_app/lib/results_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuizAppResults extends StatefulWidget {
+  const QuizAppResults({super.key});
+
+  @override
+  State<QuizAppResults> createState() => _QuizAppResultsState();
+}
+
+class _QuizAppResultsState extends State<QuizAppResults> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
