c205a4ebdc1653834e8ccc8d683e318259c85b44
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-03 21:02:04 +0530

    Flutter App Initialized

---
+++ Project-4-To-Do-App/todo/lib/to_do_scaffold.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ToDoScaffold extends StatefulWidget {
+  const ToDoScaffold({super.key});
+  @override
+  State<ToDoScaffold> createState() => _ToDoScaffold();
+}
+
+class _ToDoScaffold extends State<ToDoScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
