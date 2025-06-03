91f2ce09d1c4c1e57620f7771f8ed9b758276279
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-03 10:40:39 +0530

    Flutter: Project-3: List Of Exepense Data Initialized

--- Project-3-Expense-Tracker-App/expense_tracker/lib/tracker_page.dart
+++ Project-3-Expense-Tracker-App/expense_tracker/lib/tracker_page.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class TrackerPage extends StatefulWidget {
@@ -9,6 +10,32 @@ class TrackerPage extends StatefulWidget {
 }
 
 class _TrackerPageState extends State<TrackerPage> {
+  List<Expense> expensesList = [
+    Expense(
+      title: 'Expense-1',
+      category: Category.food,
+      amount: 5.05,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: 'Expense-2',
+      category: Category.work,
+      amount: 10.55,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: 'Expense-3',
+      category: Category.travel,
+      amount: 15.05,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: 'Expense-4',
+      category: Category.leisure,
+      amount: 20.55,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
