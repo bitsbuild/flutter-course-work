28980d10ee6623fb2d5776d864f2617e315ffc9e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-04 21:38:03 +0530

    Flutter: Project-3: Basic Expense Tracker App Structure Established

--- Project-3-Expense-Tracker-App/expense_tracker/lib/expense_tracker_class.dart
+++ Project-3-Expense-Tracker-App/expense_tracker/lib/expense_tracker_class.dart
@@ -1,9 +1,11 @@
 import 'package:uuid/uuid.dart';
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   String id;
   String title;
-  String category;
+  Category category;
   double amount;
   Expense({required this.title, required this.category, required this.amount})
     : id = Uuid().v4();
--- Project-3-Expense-Tracker-App/expense_tracker/lib/expense_tracker_scaffold.dart
+++ Project-3-Expense-Tracker-App/expense_tracker/lib/expense_tracker_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expense_tracker_class.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseTrackerScaffold extends StatefulWidget {
@@ -7,8 +8,89 @@ class ExpenseTrackerScaffold extends StatefulWidget {
 }
 
 class _ExpenseTrackerScaffoldState extends State<ExpenseTrackerScaffold> {
+  List<Color> col = [Color(0xff8E0E00), Color(0xff1F1C18)];
+  List<Expense> li = [
+    Expense(title: 'Trekking', category: Category.leisure, amount: 10),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        actions: [
+          IconButton(
+            onPressed:
+                () => {
+                  showModalBottomSheet(
+                    isScrollControlled: false,
+                    context: context,
+                    builder: (ctx) {
+                      return SizedBox.expand(
+                        child: Center(child: Text('Hello')),
+                      );
+                    },
+                  ),
+                },
+            icon: Icon(Icons.add_box, color: Colors.white, size: 30),
+          ),
+        ],
+        title: Text(
+          'Flutter Expense Tracker',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(gradient: LinearGradient(colors: col)),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: col),
+              border: Border.all(color: Colors.white, width: 3),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Center(
+                      child: Text(
+                        '1-1-1-1-1:::::Analytics:::::1-1-1-1-1',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 300,
+                    child: ListView.builder(
+                      itemCount: li.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Center(child: Text(li[index].title)),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
