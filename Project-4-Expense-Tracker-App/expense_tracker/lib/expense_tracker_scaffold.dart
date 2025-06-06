e6826f471f98d9c7fb7a64748e7642aca61d8a0e
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-06-07 00:01:05 +0530

    Flutter: Project-4: Expense Tracker AppBar With Add Expense Feature And Title Completed

--- Project-4-Expense-Tracker-App/expense_tracker/lib/expense_tracker_scaffold.dart
+++ Project-4-Expense-Tracker-App/expense_tracker/lib/expense_tracker_scaffold.dart
@@ -1,10 +1,11 @@
 import 'package:expense_tracker/expense_tracker_appbar.dart';
 import 'package:expense_tracker/expense_tracker_body.dart';
+import 'package:expense_tracker/expense_tracker_class.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseTrackerScaffold extends StatefulWidget {
-  const ExpenseTrackerScaffold({super.key});
-
+  late ValueChanged<Expense> functionAdd;
+  ExpenseTrackerScaffold({super.key, required this.functionAdd});
   @override
   State<ExpenseTrackerScaffold> createState() => _ExpenseTrackerScaffoldState();
 }
@@ -12,6 +13,9 @@ class ExpenseTrackerScaffold extends StatefulWidget {
 class _ExpenseTrackerScaffoldState extends State<ExpenseTrackerScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: expenseTrackarAppBar, body: expenseTrackerBody);
+    return Scaffold(
+      appBar: ExpenseTrackerAppbar(functionAdd: widget.functionAdd),
+      body: expenseTrackerBody,
+    );
   }
 }
--- Project-4-Expense-Tracker-App/expense_tracker/lib/main.dart
+++ Project-4-Expense-Tracker-App/expense_tracker/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expense_tracker_class.dart';
 import 'package:expense_tracker/expense_tracker_scaffold.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,18 @@ class ExpenseTracker extends StatefulWidget {
   State<ExpenseTracker> createState() => _ExpenseTrackerState();
 }
 
+List<Expense> expenses = [];
+void listAdd(Expense e) {
+  print(expenses.length);
+  expenses.add(e);
+  print(expenses.length);
+}
+
 class _ExpenseTrackerState extends State<ExpenseTracker> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ExpenseTrackerScaffold(),
+      home: ExpenseTrackerScaffold(functionAdd: listAdd),
       debugShowCheckedModeBanner: false,
       title: 'Expense Tracker',
     );
